9850442099ee986b8d47b80ec94ea55f2ecf98d2
Daniel Rooth2 <daniel.rooth2@hva.nl>
2016-10-27 12:01:09 +0200

    Bewoner, base roaming

---
+++ Spooky/Assets/Daniel/Scripts/testerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testerScript : MonoBehaviour {
+
+    public GameObject target;
+    public Transform testLocation;
+    public residentMovement residentMovementScript;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            residentMovementScript.investigate(testLocation.position);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            residentMovementScript.targetRoom = "kitchen";
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            residentMovementScript.targetRoom = "lobby";
+        }
+
+    }
+}
