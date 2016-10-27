65ad5ca29d22b2f89d209d47159fb2d81c489281
Rick Dorr <Rick.nl@hotmail.com>
2016-10-27 14:58:30 +0200

    rick shit

--- Spooky/Assets/Tim/Scripts/Button.cs
+++ Spooky/Assets/Tim/Scripts/Button.cs
@@ -3,35 +3,41 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class Button : MonoBehaviour {
-
+    public Color highlightColor;
+    Color textColor;
     Text text;
+
 	// Use this for initialization
 	void Start () {
         text = GetComponent<Text>();
+        textColor = text.color;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     public void Highlight()
     {
-        text.color = Color.white;
+        text.color = Color.red;
     }
 
     public void StopHighlight()
     {
-        text.color = new Color(56.0f / 255.0f, 56.0f / 255.0f, 56.0f / 255.0f, 1);
+        text.color = textColor;
     }
 
     public void Click()
     {
         if (gameObject.name == "Retry Text") {
-            Application.LoadLevel("Game");
+            Application.LoadLevel("Tim");
         }
         else if (gameObject.name == "Exit Text") {
-            Application.LoadLevel("Menu");
+            Application.LoadLevel("Marc");
+        }
+        else if (gameObject.name == "Next Level Text")
+        {
+            Application.LoadLevel("Marc");
+        }
+        else if (gameObject.name == "Main Menu Text")
+        {
+            Application.LoadLevel("Marc");
         }
 
     }
