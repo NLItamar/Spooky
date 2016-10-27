6348dd5284d33968a4c56570076ec957c7f8dc7c
TimRoberson <timroberson@live.nl>
2016-10-27 11:52:07 +0200

    Player controller in Scene "Tim": Move/Rotate Camera Game Over State in Scene "GameOver": Menu for when you lose Level Complete State in Scene "Tim": Use script to activate gameobject Level Complete

---
+++ Spooky/Assets/Tim/Scripts/Button.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+    Text text;
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Highlight()
+    {
+        text.color = Color.white;
+    }
+
+    public void StopHighlight()
+    {
+        text.color = new Color(56.0f / 255.0f, 56.0f / 255.0f, 56.0f / 255.0f, 1);
+    }
+
+    public void Click()
+    {
+        if (gameObject.name == "Retry Text") {
+            Application.LoadLevel("Game");
+        }
+        else if (gameObject.name == "Exit Text") {
+            Application.LoadLevel("Menu");
+        }
+
+    }
+}
